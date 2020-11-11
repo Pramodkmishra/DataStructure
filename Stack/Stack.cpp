42357337d3d4cdfd053a8dfae05cc68dab8367ed
pmdkmis@gmail.com <pmdkmis@gmail.com>
2020-11-11 09:59:58 +0530

    stack added

---
+++ Stack/Stack.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+template<class x>
+class Stack
+{
+private:
+x *a;
+int top=-1,size;
+public:
+void push(x data);
+x pop();
+int get_size();
+};
+Stack::Stack(int s)
+{
+size=s;
+a=new
+}
+void push(x data)
+{
+
+}
